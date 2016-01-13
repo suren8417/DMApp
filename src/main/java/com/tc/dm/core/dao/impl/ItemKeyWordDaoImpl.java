96f9e5c10ee830a1017eb4cf55bf3b0136a23024
mathotkadar <mathotkadar@gmail.com>
2016-01-13 17:52:15 +0530

    removing depricated classes

--- src/main/java/com/tc/dm/core/dao/impl/ItemKeyWordDaoImpl.java
+++
@@ -1,8 +0,0 @@
-package com.tc.dm.core.dao.impl;
-
-import com.tc.dm.core.entities.KeyWord;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ItemKeyWordDaoImpl extends GenericDaoJpaImpl<Long, KeyWord> {
-}
--- src/main/java/com/tc/dm/core/dao/impl/ItemTypeDaoImpl.java
+++
@@ -1,8 +0,0 @@
-package com.tc.dm.core.dao.impl;
-
-import com.tc.dm.core.entities.ItemType;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ItemTypeDaoImpl extends GenericDaoJpaImpl<Long, ItemType> {
-}
